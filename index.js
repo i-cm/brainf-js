60a7506178ceb294cf7cfeeaab11ceef9f1386ec
i-cm <74533816+i-cm@users.noreply.github.com>
2022-10-13 21:45:59 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function runbf(text) {
+  a = []
+  for (let i = 0; i < 100; i++) {
+    a[i] = 0
+  }
+  index = 0
+  n = 0
+  regex = /+-<>,.[]/
+  return eval(text.match(regex).replace("+", "a[index]++; n++; ").replace("-", "a[index]--; n--; ").replace("<", "index--; n = a[index]; ").replace(">", "index++; n = a[index]; ").replace("[", "while (n != 0) {").replace("]", "} ").replace(",", "n = new Number(Prompt(\"Enter the number\", \"0\")); ").replace(".", "alert(n)"))
+}
