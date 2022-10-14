c4ecd7b3d1921586484aba80d99a8c266db3092f
i-cm <74533816+i-cm@users.noreply.github.com>
2022-10-14 11:13:16 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function runbf(text) {
   }
   index = 0
   n = 0
-  return eval(text.replace("+", "a[index]++; n++; ").replace("-", "a[index]--; n--; ").replace("<", "index--; n = a[index]; ").replace(">", "index++; n = a[index]; ").replace("[", "while (n != 0) {").replace("]", "} ").replace(",", "n = new Number(Prompt(\"Enter the number\", \"0\")); ").replace(".", "alert(n)"))
+  return eval(text.replace("+", "a[index]++; n++; ").replace("-", "a[index]--; n--; ").replace("<", "index--; n = a[index]; ").replace(">", "index++; n = a[index]; ").replace("[", "while (n != 0) {").replace("]", "} ").replace(",", "n = new Number(Prompt(\"Enter the number\", \"0\")); ").replace(".", "alert(n); "))
 }
